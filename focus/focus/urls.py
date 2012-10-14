7ea15c55c6b3149e7dca59343731a12b7e7accb7
Justin Edmund <justin@jedmund.com>
2012-10-14 15:06:28 -0700

    Split urls.py for apps

--- focus/focus/urls.py
+++ focus/focus/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^contacts/$', 'contacts.views.index'),
-    url(r'^contacts/(?P<poll_id>\d+)/$', 'contacts.views.person'),
-    url(r'^contacts/(?P<poll_id>\d+)/edit/$', 'contacts.views.edit'),
-    url(r'^contacts/create/$', 'contacts.views.create'),
+    url(r'^contacts/', include('contacts.urls')),
+    url(r'^groups/', include('groups.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
