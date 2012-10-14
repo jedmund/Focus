6a884b7d120d8eb4f391e7c0d9981699ae6141dd
Justin Edmund <justin@jedmund.com>
2012-10-13 19:52:01 -0700

    Updated models to use localflavor ext

--- focus/contacts/models.py
+++ focus/contacts/models.py
@@ -1,20 +1,52 @@
 from django.db import models
+from django.contrib.localflavor.us.models import USStateField, PhoneNumberField
 
 class Person(models.Model):
-    first_name=models.CharField(max_length=200)
-    last_name=models.CharField(max_length=200)
 
-    birthdate=models.DateField('birthdate')
-    occupation=models.CharField(max_length=400)
-    employer=models.CharField(max_length=400)
-    ethnicity=models.CharField(max_length=40)
+    # Ethnicity mapping
+    CAUCASIAN        = 'CA'
+    AFRICAN_AMERICAN = 'AA'
+    ASIAN_PACIFIC    = 'AP'
+    HISPANIC         = 'HS'
+    AMERICAN_INDIAN  = 'AI'
+    SUBCONTINENTAL   = 'SA'
 
-    email=models.CharField(max_length=255)
-    home_phone_no=models.CharField(max_length=50)
-    mobile_phone_no=models.CharField(max_length=50)
-    work_phone_no=models.CharField(max_length=50)
+    ETHNICITY_CHOICES = (
+        (CAUCASIAN,        'Caucasian'),
+        (AFRICAN_AMERICAN, 'African-American'),
+        (ASIAN_PACIFIC,    'Asian/Pacific Islander'),
+        (HISPANIC,         'Hispanic'),
+        (AMERICAN_INDIAN,  'American Indian/Alaskan Native'),
+        (SUBCONTINENTAL,   'Subcontinental Asian')
+    )
 
-    city=models.CharField(max_length=255)
-    state=models.CharField(max_length=2)
+    # Method mapping
+    INTERNAL = 1;
+    APPLIED  = 2;
 
-    notes=models.TextField()
+    METHOD_CHOICES = (
+        (INTERNAL, 'Added internally'),
+        (APPLIED,  'Applied via web form')
+    );
+
+
+    first_name = models.CharField(max_length=200)
+    last_name  = models.CharField(max_length=200)
+
+    birthdate  = models.DateField('birthdate')
+    occupation = models.CharField(max_length=400, blank=True, null=True)
+    employer   = models.CharField(max_length=400, blank=True, null=True)
+    ethnicity  = models.CharField(max_length=40, choices=ETHNICITY_CHOICES, default=CAUCASIAN)
+
+    email           = models.CharField(max_length=255, blank=True, null=True)
+    home_phone_no   = PhoneNumberField(models.CharField('Home Phone #', blank=True, null=True))
+    mobile_phone_no = PhoneNumberField(models.CharField('Mobile Phone #', blank=True, null=True))
+    work_phone_no   = PhoneNumberField(models.CharField('Work Phone #', blank=True, null=True))
+
+    city  = models.CharField(max_length=255)
+    state = USStateField()
+
+    notes = models.TextField(blank=True, null=True)
+
+    add_date   = models.DateTimeField('Date added', auto_now_add=True)
+    add_method = models.IntegerField('Method', max_length=2, choices=METHOD_CHOICES, default=INTERNAL)
--- focus/contacts/views.py
+++ focus/contacts/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from contacts.models import Person
+from django.http import HttpResponse
+
+def index(request):
+    people_list = Poll.objects.all().order_by('-last_name')[:5]
+    output = ', '.join([p.first_name for p in people_list])
+    return HttpResponse(output)
+
+def person(request):
+    return HttpResponse("This is a person.")
+
+def create(request):
+    return HttpResponse("Add a new person.")
+
+def edit(request):
+    return HttpResponse("Edit this person.")
