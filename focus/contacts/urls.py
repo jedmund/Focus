eafe8b3234914bed54999c618591fb2f11a0ff55
Justin Edmund <justin@jedmund.com>
2012-10-15 02:33:45 -0700

    Contacts/urls.py

--- focus/contacts/urls.py
+++ focus/contacts/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('contacts.views',
     url(r'^$', 'index'),
-    url(r'^(?P<person_id>\d+)/$', 'person'),
-    url(r'^(?P<person_id>\d+)/edit/$', 'edit'),
-    url(r'^create/$', 'create'),
+    url(r'^(?P<person_id>\d+)/$', 'person', name='person'),
+    url(r'^(?P<person_id>\d+)/edit/$', 'edit', name='edit'),
+    url(r'^create/$', 'create', name='create'),
 )
