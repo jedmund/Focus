73cc9942ab5c838368cf8edf7baeb1f91b006842
Justin Edmund <justin@jedmund.com>
2012-10-15 02:34:10 -0700

    Backend jiggering pagination and POSTing

--- focus/contacts/views.py
+++ focus/contacts/views.py
@@ -1,16 +1,31 @@
-from django.template import Context, loader
+from django.template import RequestContext, Context, loader
 from django.shortcuts import render_to_response, get_object_or_404
 from contacts.models import Person, PersonForm
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from datetime import date
 
 def index(request):
-    people_list = Person.objects.all().order_by('last_name')[:5]
-    for person in people_list:
+    people = Person.objects.all().order_by('last_name')
+    for person in people:
         person.age = int((date.today() - person.birthdate).total_seconds()/60/60/24/365)
+
+    paginator = Paginator(people, 5)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+
     t = loader.get_template('contacts/index.html')
     c = Context({
-        'people': people_list,
+        'people': contacts,
+        'view': 'contacts'
     })
     return HttpResponse(t.render(c))
 
@@ -22,13 +37,30 @@ def person(request, person_id):
 
     # Determine age from boolean
     p.gender = 'Male' if 1 else 'Female'
-    return render_to_response('contacts/person.html', {'p': p})
+    return render_to_response('contacts/person.html', {'p': p, 'view': 'contacts'})
 
 def edit(request, person_id):
     p = get_object_or_404(Person, pk=person_id)
     form = PersonForm(instance=p)
-    return render_to_response('contacts/edit.html', {'p': p, 'form': form})
+    return render_to_response('contacts/edit.html', {'p': p, 'form': form, 'view': 'contacts'})
 
 def create(request):
-    form = PersonForm()
-    return render_to_response('contacts/edit.html', {'form': form})
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        print form.is_valid()
+        if form.is_valid():
+            p = form.save()
+            print p.id
+            return HttpResponseRedirect(reverse('contacts.views.person', kwargs={'person_id': p.id}))
+        else:
+            print form
+            return render_to_response('contacts/edit.html', {
+                'form': form,
+                'view': 'contacts'
+            }, context_instance=RequestContext(request))
+    else:
+        form = PersonForm()
+        return render_to_response('contacts/edit.html', {
+            'form': form,
+            'view': 'contacts'
+        }, context_instance=RequestContext(request))
